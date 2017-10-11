6597c8122e061104de90b16b4a197db3c82f7333
sanjeet <you@example.com>
2017-10-11 18:17:36 +0530

    one

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.precompile += %w(*.js)
 Rails.application.config.assets.precompile += %w( loginsignup.css )
 Rails.application.config.assets.precompile += %w( loginsignup.js )
 
