9c2ba78665c966c17bbb19b815860c72d56a7a15
sanjeet <you@example.com>
2017-10-11 20:55:01 +0530

    notes title + modal blackout updated

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -73,6 +73,6 @@ class NotesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def note_params
-      params.require(:note).permit(:content,:user_id)
+      params.require(:note).permit(:title,:content,:user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005085745) do
+ActiveRecord::Schema.define(version: 20171011142142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,6 +129,7 @@ ActiveRecord::Schema.define(version: 20171005085745) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
   add_index "notes", ["user_id"], name: "index_notes_on_user_id", using: :btree
